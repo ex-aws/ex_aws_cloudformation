9245ccacfb4181202b48d122c802a72429bafb39
Ben Wilson <benwilson512@gmail.com>
2018-06-27 17:45:31 -0400

    version bump

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ExAws.Cloudformation.Mixfile do
   use Mix.Project
 
-  @version "2.0.0"
+  @version "2.0.1"
   @service "cloudformation"
   @url "https://github.com/ex-aws/ex_aws_#{@service}"
 
